7644c537f4e62f037f38f12bd95f32a3587d1e8b
sagnickm <55552916+sagnickm@users.noreply.github.com>
2019-10-30 06:01:13 -0700

    Update calc_mie.py

--- calc_mie.py
+++ calc_mie.py
@@ -1,18 +1,78 @@
 import numpy as np
-import sys
 pi=np.pi
 from numba import jit 
-
 #@jit(nopython=True, cache=True)
+
 def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
-    EPSILON_MIE = 1e-7
+    #     THIS SUBROUTINE COMPUTES MIE SCATTERING BY A STRATIFIED SPHERE,
+#    I.E. A PARTICLE CONSISTING OF A SPHERICAL CORE SURROUNDED BY A
+#    SPHERICAL SHELL.  THE BASIC CODE USED WAS THAT DESCRIBED IN THE
+#    REPORT: " SUBROUTINES FOR COMPUTING THE PARAMETERS OF THE
+#    ELECTROMAGNETIC RADIATION SCATTERED BY A SPHERE " J.V. DAVE,
+#    I B M SCIENTIFIC CENTER, PALO ALTO , CALIFORNIA.
+#    REPORT NO. 320 - 3236 .. MAY 1968 .
+#
+#    THE MODIFICATIONS FOR STRATIFIED SPHERES ARE DESCRIBED IN
+#       TOON AND ACKERMAN, APPL. OPTICS, IN PRESS, 1981
+#
+#    THE PARAMETERS IN THE CALLING STATEMENT ARE DEFINED AS FOLLOWS :
+#      RO IS THE OUTER (SHELL) RADIUS;
+#      R  IS THE CORE RADIUS;
+#      RFR, RFI  ARE THE REAL AND IMAGINARY PARTS OF THE SHELL INDEX
+#          OF REFRACTION IN THE FORM (RFR - I* RFI);
+#      RE2, TMAG2  ARE THE INDEX PARTS FOR THE CORE;
+#          ( WE ASSUME SPACE HAS UNIT INDEX. )
+#      THET: ANGLE IN DEGREES BETWEEN THE DIRECTIONS OF THE INCIDENT
+#          AND THE SCATTERED RADIATION.  THETD(J) IS< OR= 90.0
+#          IF THETD(J) SHOULD HAPPEN TO BE GREATER THAN 90.0, ENTER WITH
+#          SUPPLEMENTARY VALUE, SEE COMMENTS BELOW ON ELTRMX;
+#      JX: TOTAL NUMBER OF THETD FOR WHICH THE COMPUTATIONS ARE
+#          REQUIRED.  JX SHOULD NOT EXCEED IT UNLESS THE DIMENSIONS
+#          STATEMENTS ARE APPROPRIATEDLY MODIFIED;
+#
+#      THE DEFINITIONS FOR THE FOLLOWING SYMBOLS CAN BE FOUND IN"LIGHT
+#          SCATTERING BY SMALL PARTICLES,H.C.VAN DE HULST, JOHN WILEY '
+#          SONS, INC., NEW YORK, 1957" .
+#      THIS MODULE GIVES BACK QEXT,QSCAT & CTBRQS
+#      QEXT: EFFICIENCY FACTOR FOR EXTINCTION,VAN DE HULST,P.14 ' 127.
+#      QSCAT: EFFICIENCY FACTOR FOR SCATTERING,V.D. HULST,P.14 ' 127.
+#      CTBRQS: AVERAGE(COSINE THETA) * QSCAT,VAN DE HULST,P.128
+#      ELTRMX(I,J,K): ELEMENTS OF THE TRANSFORMATION MATRIX F,V.D.HULST
+#          ,P.34,45 ' 125. I=1: ELEMENT M SUB 2..I=2: ELEMENT M SUB 1..
+#          I = 3: ELEMENT S SUB 21.. I = 4: ELEMENT D SUB 21..
+#      ELTRMX(I,J,1) REPRESENTS THE ITH ELEMENT OF THE MATRIX FOR
+#          THE ANGLE THETD(J).. ELTRMX(I,J,2) REPRESENTS THE ITH ELEMENT
+#          OF THE MATRIX FOR THE ANGLE 180.0 - THETD(J) ..
+#      QBS IS THE BACK SCATTER CROSS SECTION.
+#
+#      IT: IS THE DIMENSION OF THETD, ELTRMX, CSTHT, PI, TAU, SI2THT,
+#          IT MUST CORRESPOND EXACTLY TO THE SECOND DIMENSION OF ELTRMX.  
+#      nacap IS THE DIMENSION OF ACAP 
+#          IN THE ORIGINAL PROGRAM THE DIMENSION OF ACAP WAS 7000.
+#          FOR CONSERVING SPACE THIS SHOULD BE NOT MUCH HIGHER THAN
+#          THE VALUE, N=1.1*(NREAL**2 + NIMAG**2)**.5 * X + 1
+#      WVNO: 2*PI / WAVELENGTH
+#
+#    ALSO THE SUBROUTINE COMPUTES THE CAPITAL A FUNCTION BY MAKING USE O
+#    DOWNWARD RECURRENCE RELATIONSHIP.
+#
+#      TA(1): REAL PART OF WFN(1).  TA(2): IMAGINARY PART OF WFN(1).
+#      TA(3): REAL PART OF WFN(2).  TA(4): IMAGINARY PART OF WFN(2).
+#      TB(1): REAL PART OF FNA.     TB(2): IMAGINARY PART OF FNA.
+#      TC(1): REAL PART OF FNB.     TC(2): IMAGINARY PART OF FNB.
+#      TD(1): REAL PART OF FNAP.    TD(2): IMAGINARY PART OF FNAP.
+#      TE(1): REAL PART OF FNBP.    TE(2): IMAGINARY PART OF FNBP.
+#      FNAP, FNBP  ARE THE PRECEDING VALUES OF FNA, FNB RESPECTIVELY.
+# **********************************************************************
+    EPSILON_MIE = 1e-7  ## Tolerance for convergence
+
     nacap, IT = 1000000, 1
     
-    ACAP=np.zeros(shape=(nacap),dtype=np.complex64) 
-    W=np.zeros(shape=(3,nacap),dtype=np.complex64)
-    WFN=np.zeros(shape=(2),dtype=np.complex64)
-    Z=np.zeros(shape=(4),dtype=np.complex64)
-    U=np.zeros(shape=(8),dtype=np.complex64)
+    ACAP=np.zeros(shape=(nacap),dtype=complex) 
+    W=np.zeros(shape=(3,nacap),dtype=complex)
+    WFN=np.zeros(shape=(2),dtype=complex)
+    Z=np.zeros(shape=(4),dtype=complex)
+    U=np.zeros(shape=(8),dtype=complex)
     T= np.zeros(shape=(5))    
     TA=np.zeros(shape=(4))     
     TB=np.zeros(shape=(2))        
@@ -25,19 +85,18 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
     THETD=np.full((IT),THET) 
     SI2THT=np.zeros(shape=(IT))   
     ELTRMX=np.zeros(shape=(4,IT,2))
-    
     IFLAG = 1
     if  R/RO < 1e-6:    
         IFLAG = 2
-    if  JX > IT:    
-        print('THE VALUE OF THE ARGUMENT JX IS GREATER THAN IT')
-        print('PLEASE READ COMMENTS')
-        return
+    if  JX > IT:
+        raise Exception('THE VALUE OF THE ARGUMENT JX IS GREATER THAN IT. PLEASE READ COMMENTS.')
+        
+            
     RF =  complex(RFR,-RFI)
     RC =  complex( RE2,-TMAG2 )
     X  =  RO * WVNO
-    K1 =  (RC * WVNO)
-    K2 =  (RF * WVNO)
+    K1 =  RC * WVNO
+    K2 =  RF * WVNO
     K3 =  complex( WVNO, 0.0 )
     Z[0] =  K2 * RO
     Z[1] =  K3 * RO
@@ -50,9 +109,7 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
     RRF  =  1.0 / RF
     RX   =  1.0 / X
     RRFX =  RRF * RX
-    T[0] = ( ( X**2 ) * ( RFR**2 + RFI**2 ))
-    T[0]=np.sqrt(T[0])
-
+    T[0] = np.sqrt( ( X**2 ) * ( RFR**2 + RFI**2 ))
     NMX1 = int( 1.10 * T[0])
     if  NMX1 > nacap-1 :
         istatus=-1
@@ -78,7 +135,7 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
             THETD[J] =  abs( THETD[J] )
     
         if  THETD[J] < 90.0:
-            T[0]     =  ( pi * THETD[J] ) / 180.0
+            T[0]     =  ( 3.14159265359 * THETD[J] ) / 180.0
             CSTHT[J] =  np.cos( T[0])
             SI2THT[J] =  1.0 - CSTHT[J]**2
             continue
@@ -87,9 +144,8 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
             SI2THT[J] =  1.0
             continue
         if  THETD[J] > 90.0 :
-            print(THETD[J],' THE VALUE OF THE SCATTERING ANGLE IS GREATER THAN 90.0 DEGREES.')
-            print('Please read comments')
-            #sys.exit()
+            raise Exception(' THE VALUE OF THE SCATTERING ANGLE IS GREATER THAN 90.0 DEGREES. PLEASE READ COMMENTS')
+        
     for J in range(JX):
         PI[0,J]  =  0.0
         PI[1,J]  =  1.0
@@ -166,6 +222,7 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
         TB[1]=FNA.imag
         TC[0]=FNB.real
         TC[1]=FNB.imag
+        
     FNAP = FNA
     FNBP = FNB
     TD[0]=FNAP.real
@@ -214,6 +271,7 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
         WFN[1] =  T[0] * RX * WFN[0]  -  WM1
         TA[2]  =  WFN[1].real
         
+        
         if IFLAG != 2:
             DH2  =  - N / Z[1]  +  1.0 / ( (N / Z[1]) - DH2 )
             DH4  =  - N / Z[3]  +  1.0 / ( (N / Z[3]) - DH4 )
@@ -246,17 +304,19 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
         FN2  =  ( TC2 * TA[2]  -  TA[0] ) /  ( TC2 * WFN[1] - WFN[0] )
         M    =  int(WVNO * R)
         if  N >= M : 
-            if IFLAG ==2 or IFLAG == 1: 
+            if IFLAG ==2: 
                 FNA  =  FN1
                 FNB  =  FN2
                 TB[0]=FNA.real
                 TB[1]=FNA.imag
                 TC[0]=FNB.real
                 TC[1]=FNB.imag
+
             if IFLAG != 2:    
-                if  (abs(  ( FN1-FNA ) / FN1  ) < EPSILON_MIE)  & (abs(  ( FN2-FNB ) / FN2  )  < EPSILON_MIE) :
-                    IFLAG = 2
-            
+                if  abs(  ( FN1-FNA ) / FN1  ) < EPSILON_MIE:
+                    if abs(  ( FN2-FNB ) / FN2  )  < EPSILON_MIE :
+                        IFLAG = 2
+                    
             
      
         T[4]  =  N
@@ -273,7 +333,7 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
 
         T[1]    =  N * (N+1)
         T[0]    =  T[2] / T[1]
-        K=N
+        K=int(N)
         for J in range(JX):
             ELTRMX[0,J,0] += T[0]*(TB[0]*PI[2,J]+TC[0]*TAU[2,J])
             ELTRMX[1,J,0] += T[0]*(TB[1]*PI[2,J]+TC[1]*TAU[2,J])
@@ -309,14 +369,15 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
         
         else:
             break 
-    if  N > NMX2 :
+    if  N >= NMX2 :
+        #print(T[3],NMX2)
         istatus=-1
         return 0,0,0,istatus   
     for J in range(JX):
         for K in range(2):
             for I in range(4):
                 T[I]  =  ELTRMX[I,J,K]
-  
+    
             ELTRMX[1,J,K]  =      T[0]**2  +  T[1]**2
             ELTRMX[0,J,K]  =      T[2]**2  +  T[3]**2
             ELTRMX[2,J,K]  =  T[0] * T[2]  +  T[1] * T[3]
@@ -327,4 +388,5 @@ def mie_calc(RO, RFR, RFI, THET, JX, R, RE2, TMAG2, WVNO ):
     QSCAT   =  QSCAT * T[0]
     CTBRQS  =  2.0 * CTBRQS * T[0]           
     istatus = 0
-    return QEXT,QSCAT,CTBRQS,istatus
+    
+    return QEXT,QSCAT,CTBRQS,istatus  # THE SUBROUTINE RETURNS THESE FOUR QUANTITIES INTERPRETATIONS OF WHICH ARE PRESENT IN DESCRIPTION OF THE SUBROUTINE IN THE BEGINNING
