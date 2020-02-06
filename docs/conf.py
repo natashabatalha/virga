545ea3462d3b0bb7b953821e5d0e07a580473dec
Natasha Batalha <natasha.e.batalha@gmail.com>
2020-02-05 21:17:39 -0800

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,84 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('/Users/nbatalh1/Documents/codes/VIRGA/virga/'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'virga'
+copyright = '2020, Natasha E. Batalha & Mark Marley'
+author = 'Natasha E. Batalha & Mark Marley'
+
+# The full version, including alpha/beta/rc tags
+release = 'v0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'nbsphinx']
+nbsphinx_allow_errors = True
+nbsphinx_execute = 'never'
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','**.ipynb_checkpoints']
+
+pygments_style = 'sphinx'
+todo_include_todos = True
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+htmlhelp_basename = 'virgadoc'
+
+nbsphinx_prolog = """
+{% set docname = env.doc2path(env.docname, base=None) %}
+.. note::  `Download full notebook here <https://github.com/natashabatalha/virga/tree/master/docs/{{ docname }}>`_
+"""
+
+
+def setup(app):
+    app.add_stylesheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')
+    app.add_javascript('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js')
+    app.add_javascript('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')
+
+
---
+++ docs/index.rst
@@ -0,0 +1,59 @@
+.. virga documentation master file, created by
+   sphinx-quickstart on Wed Feb  5 16:35:40 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+.. raw:: html
+
+   <div align="center">
+   <img src="_images/logo.png" width="450px">
+   </img>
+   <br/>
+   </div>
+   <br/><br/>
+
+``virga`` computes cloud profiles for exoplanet atmospheres
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   Installation <installation>
+   The Tutorials <tutorials>
+   Github <https://github.com/natashabatalha/virga>
+   The Code <virga>
+
+.. raw:: html 
+
+	 <div class="row">
+	  <div class="col-md-3">
+	    <div class="thumbnail">
+	      <a href="installation.html">
+	        <img src="_images/installation.png" alt="installation" style="width:100%">
+	      </a>
+	    </div>
+	  </div>
+	  <div class="col-md-3">
+	    <div class="thumbnail">
+	      <a href="tutorials.html">
+	        <img src="_images/tutorials.png" alt="tutorials" style="width:100%">
+	      </a>
+	    </div>
+	  </div>
+	  <div class="col-md-3">
+	    <div class="thumbnail">
+	      <a href="picaso.html">
+	        <img src="_images/code.png" alt="code" style="width:100%">
+	      </a>
+	    </div>
+	  </div>
+	</div>
+	</div>
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
