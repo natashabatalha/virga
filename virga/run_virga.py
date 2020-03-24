9b7ba1312e018c642909f374d06e589a794c76e5
caoimherooney11 <caoimherooney27@gmail.com>
2020-03-24 13:07:04 +0000

    made a notebook to demonstrate differences in mmr results

--- virga/run_virga.py
+++ virga/run_virga.py
@@ -9,31 +9,50 @@ from bokeh.plotting import show, figure
 
 #   locate data
 mieff_directory = "~/Documents/codes/all-data/mieff_files"
-TP_directory = "~/Documents/codes/all-data/Mark_data/"
-filenames = ["t1000g1000nc_m0.0.dat", "t1500g1000nc_m0.0.dat", "t1700g1000f3_m0.0k.dat", \
-                "t200g3160nc_m0.0.dat", "t2400g3160nc_m0.0.dat"]
-filename = TP_directory + filenames[0]
+fsed = 1.
+Mark_data = True
+refine_TP = True
+quick_stop = True
 
-#   define atmosphere properties
-df = pd.read_csv(filename, delim_whitespace=True, usecols=[1,2], header=None)
-df.columns = ["pressure", "temperature"]
-pressure = np.array(df["pressure"])[1:]
-temperature = np.array(df["temperature"])[1:]
-grav = df["pressure"][0] * 100 
-kz = 1e9
-fsed = 0.05
+if Mark_data:
+    TP_directory = "~/Documents/codes/all-data/Mark_data/"
+    filenames = ["t1000g1000nc_m0.0.dat", "t1500g1000nc_m0.0.dat", "t1700g1000f3_m0.0k.dat", \
+                    "t200g3160nc_m0.0.dat", "t2400g3160nc_m0.0.dat"]
+    filename = TP_directory + filenames[1]
+    
+    #   define atmosphere properties
+    df = pd.read_csv(filename, delim_whitespace=True, usecols=[1,2], header=None)
+    df.columns = ["pressure", "temperature"]
+    pressure = np.array(df["pressure"])[1:]
+    temperature = np.array(df["temperature"])[1:]
+    grav = df["pressure"][0] * 100 
+    kz = 1e9
+    
+    metallicity = 1 #atmospheric metallicity relative to Solar
+    mean_molecular_weight = 2.2 # atmospheric mean molecular weight
+    #get pyeddy recommendation for which gases to run
+    recommended_gases = jdi.recommend_gas(pressure, temperature,
+                                                 metallicity, mean_molecular_weight)
+    
+    a = jdi.Atmosphere([recommended_gases[0]], fsed=fsed, mh=metallicity, mmw=mean_molecular_weight)
+    a.gravity(gravity=grav, gravity_unit=u.Unit('cm/(s**2)'))
+    a.ptk(df = pd.DataFrame({'pressure':pressure, 'temperature':temperature,
+                                   'kz':kz}))
 
-metallicity = 1 #atmospheric metallicity relative to Solar
-mean_molecular_weight = 2.2 # atmospheric mean molecular weight
-#get pyeddy recommendation for which gases to run
-recommended_gases = jdi.recommend_gas(pressure, temperature,
-                                             metallicity, mean_molecular_weight)
-print(recommended_gases)
-
-a = jdi.Atmosphere(recommended_gases, fsed=fsed, mh=metallicity, mmw=mean_molecular_weight)
-a.gravity(gravity=grav, gravity_unit=u.Unit('cm/(s**2)'))
-a.ptk(df = pd.DataFrame({'pressure':pressure, 'temperature':temperature,
-                               'kz':kz}))
+else:
+    metallicity = 1 #atmospheric metallicity relative to Solar
+    mean_molecular_weight = 2.2 # atmospheric mean molecular weight
+    
+    #set the run 
+    a = jdi.Atmosphere(['MnS','Cr','MgSiO3','Fe'],
+                      fsed=fsed,mh=metallicity,
+                     mmw = mean_molecular_weight)
+    
+    #set the planet gravity
+    a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
+    
+    #Get preset pt profile for testing
+    a.ptk(df = jdi.hot_jupiter())
 
 #   verify original and new solvers give same mixing ratios
 labels = ["original", "new"]
@@ -42,20 +61,25 @@ solver = [True, False]
 output = []
 fig1, ax1 = plt.subplots()
 for i in range(2):
-    all_out = jdi.compute(a, as_dict=True, directory=mieff_directory, layers=solver[i], refine_TP=False)
+    all_out = jdi.compute(a, as_dict=True, directory=mieff_directory, layers=solver[i], refine_TP=refine_TP, 
+            quick_stop=quick_stop)
     output.append(all_out)
     pres = all_out['pressure']
     qt = all_out['cond_plus_gas_mmr'][:,0]
     qc = all_out['condensate_mmr'][:,0]
 
-    ax1.set_ylim(pres[len(pres)-1], pres[0])
     ax1.loglog(qt, pres, lines[i], label="qt " + labels[i] )
     ax1.loglog(qc, pres, lines[i], label="qc " + labels[i] )
     ax1.loglog(qt-qc, pres, lines[i], label="qv " + labels[i] )
 
+pres = output[0]["pressure"]
+qc = output[0]["condensate_mmr"][:,0]
+ax1.set_ylim(pres[len(pres)-1], pres[0])
+#ax1.set_xlim([np.max([1e-9, np.min(qc*0.9)]), np.max(qc*1.1)])
 ax1.set_ylabel("pressure")
 ax1.legend(loc="best")
 plt.show()
+import sys; sys.exit()
 
 show(jpi.opd_by_gas(output[0]))
 show(jpi.opd_by_gas(output[1]))
