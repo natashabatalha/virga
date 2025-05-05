bfbee38e91afc6b0e318f0b5978488a97e899570
kiefersv <reronex@gmail.com>
2025-05-05 13:22:37 -0500

    Fix #31

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -21,4 +21,14 @@ def test_virga_cloud():
     all_out = jdi.compute(a, as_dict=True, directory='.')
 
     assert np.isclose(np.sum(all_out['condensate_mmr']), 6.163947994805619e-05)
+    
+    # initialise atmosphere
+    a = jdi.Atmosphere('MnS', fsed=1, mh=1, mmw=2.2)
+    a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
+    a.ptk(df=jdi.hot_jupiter())
+
+    # calculate cloud profile
+    all_out = jdi.compute(a, as_dict=True, directory='.')
+
+    assert np.isclose(np.sum(all_out['condensate_mmr']), 6.163947994805619e-05)
 
